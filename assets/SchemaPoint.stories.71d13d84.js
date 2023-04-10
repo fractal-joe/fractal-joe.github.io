475229ec708ce970f08d7d772dec525b1993afdd
fractal-joe <joe.narvaez@fractalems.com>
2023-04-09 20:31:02 -0500

    first commit

---
+++ assets/SchemaPoint.stories.71d13d84.js
@@ -0,0 +1,2 @@
+import{S as t}from"./SchemaPoint.69e805f9.js";import"./index.634c1e14.js";import"./iframe.9cf4e16b.js";import"./extends.93ebdc06.js";import"./Select.ccacba44.js";import"./GlobalStyles.db7c74f9.js";import"./inheritsLoose.66087489.js";import"./index.7095c559.js";const s={title:"Example/SchemaFullPoint",component:t,tags:["docsPage"],argTypes:{}},c={args:{}},l=["Main"];export{c as Main,l as __namedExportsOrder,s as default};
+//# sourceMappingURL=SchemaPoint.stories.71d13d84.js.map
